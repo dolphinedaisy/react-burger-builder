60f1a0ae88325abf6b263c0e26f9d7ed1afe9b42
dolphinedaisy <dhwanij2@gmail.com>
2020-08-21 19:16:30 +0530

    150. Adding a Sidedrawer Toggle Button

--- src/Components/Navigation/SideDrawer/ToggleButton/ToggleButton.js
+++ src/Components/Navigation/SideDrawer/ToggleButton/ToggleButton.js
@@ -1,8 +1,13 @@
 import React from "react";
+import classes from "./ToggleButton.module.css";
 
 const toggleButton = (props) => {
     return (
-        <div onClick={props.onToggle}>Menu</div>
+        <div className={classes.ToggleBtn} onClick={props.onToggle}>
+            <div></div>
+            <div></div>
+            <div></div>
+        </div>
     );
 };
 
