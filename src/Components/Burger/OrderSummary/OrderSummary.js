f4e047db86ecd60473b2922d2ee3aa0e2504e090
dolphinedaisy <dhwanij2@gmail.com>
2020-08-21 19:44:03 +0530

    155. Using Component Lifecycle Methods

--- src/Components/Burger/OrderSummary/OrderSummary.js
+++ src/Components/Burger/OrderSummary/OrderSummary.js
@@ -1,34 +1,38 @@
-import React from "react";
+import React, {Component} from "react";
 import ReactAux from "../../../hoc/ReactAux";
 import Button from "../../Layout/UI/Button/Button";
 
-const orderSummary = (props) => {
+class OrderSummary extends Component {
 
-    const ingredients = Object.keys(props.ingredients)
-        .map((ing, i) => {
-            return (
-                <li key={'ing'+i} className="text-capitalize list-group-item d-flex justify-content-between align-items-center">
-                    {ing}
-                    <span className="badge badge-primary badge-pill">{props.ingredients[ing]}</span>
-                </li>
-            );
-        });
+    componentDidUpdate(prevProps, prevState, snapshot) {
+        console.log("************** Order summary will update ***************");
+    }
 
+    render() {
+        const ingredients = Object.keys(this.props.ingredients)
+            .map((ing, i) => {
+                return (
+                    <li key={'ing'+i} className="text-capitalize list-group-item d-flex justify-content-between align-items-center">
+                        {ing}
+                        <span className="badge badge-primary badge-pill">{this.props.ingredients[ing]}</span>
+                    </li>
+                );
+            });
+        return (
+            <ReactAux>
+                <h3>Your Order</h3>
+                <p>Delicious burger with following ingredients: </p>
+                <ul className="list-group">
+                    {ingredients}
+                </ul>
+                <p className='mt-2 font-weight-bold'>Total: {this.props.price} $</p>
+                <Button clicked={this.props.purchaseContinue}
+                        type={'Success'}>Buy</Button>
+                <Button clicked={this.props.purchaseCancel}
+                        type={'Danger'}>Cancel</Button>
+            </ReactAux>
+        );
+    }
+}
 
-    return (
-        <ReactAux>
-            <h3>Your Order</h3>
-            <p>Delicious burger with following ingredients: </p>
-            <ul className="list-group">
-                {ingredients}
-            </ul>
-            <p className='mt-2 font-weight-bold'>Total: {props.price} $</p>
-            <Button clicked={props.purchaseContinue}
-                    type={'Success'}>Buy</Button>
-            <Button clicked={props.purchaseCancel}
-                    type={'Danger'}>Cancel</Button>
-        </ReactAux>
-    );
-};
-
-export default orderSummary;
+export default OrderSummary;
--- src/Components/Layout/UI/Backdrop/Modal/Modal.js
+++ src/Components/Layout/UI/Backdrop/Modal/Modal.js
@@ -1,22 +1,36 @@
-import React from "react";
+import React, { Component } from "react";
 import classes from "./Modal.module.css";
 import ReactAux from "../../../../../hoc/ReactAux";
 import Backdrop from "../Backdrop";
 
-const modal = (props) => {
-    return (
-        <ReactAux>
-            <Backdrop clicked={props.modalClosed}
-                      show={props.show}/>
-            <div style={{
-                transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
-                opacity: props.show ? '1' : '0'
-            }}
-                 className={classes.Modal}>
-                {props.children}
-            </div>
-        </ReactAux>
-    );
-};
+class Modal extends Component {
 
-export default modal;
+    shouldComponentUpdate(nextProps, nextState, nextContext) {
+        console.log("-------------- inside shouldComponentUpdate Modal.js ---------------");
+        // agal nu show and amna nu show jo alag hoy to j agal render ni permission apvani
+        return nextProps.show !== this.props.show;
+    }
+
+    componentDidUpdate(prevProps, prevState, snapshot) {
+        console.log('-------------- inside componentDidUpdate Modal.js ---------------')
+    }
+
+    render() {
+        return (
+            <ReactAux>
+                <Backdrop clicked={this.props.modalClosed}
+                          show={this.props.show}/>
+                <div style={{
+                    transform: this.props.show ? 'translateY(0)' : 'translateY(-100vh)',
+                    opacity: this.props.show ? '1' : '0'
+                }}
+                     className={classes.Modal}>
+                    {this.props.children}
+                </div>
+            </ReactAux>
+        );
+    }
+
+}
+
+export default Modal;
